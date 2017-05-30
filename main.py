0b19e287a92e404b6c6ba2d421591b913e6d621f
mainakchain <mainakchain21@gmail.com>
2017-05-30 19:18:51 +0530

    framework

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+__version__ = 1.0
+
 from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 
 
 class RootWidget(FloatLayout):
-    pass
+	pass
 
-
-class MainApp(App):
-    def build(self):
-        return RootWidget()
+class MainApp(App):		
+	def build(self):
+		return RootWidget()
 
 if __name__ == '__main__':
     MainApp().run()
