0cc500a8b627f3649c2fc646d5bebb76717db43e
sohit-nayak <sohit.nayak@gmail.com>
2017-06-01 11:09:51 +0530

    internet popup added

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+from kivy.properties import StringProperty
+
+class InternetPopup(Popup):
+	text = StringProperty()
 
 
 class RootWidget(TabbedPanel):
-    pass
+    
+    def internet_popup(self, *args):
+    	InternetPopup().open()
+
 
 class DssApp(App):
     def build(self):
         return RootWidget()
+
 
 if __name__ == '__main__':
     DssApp().run()
